ffabfaf233678d9224b3135c52d40f90400778e9
luit74 <luit.dusad74@gmail.com>
2023-09-28 14:27:21 +0530

    limitation to the new button

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard
+// let firstCard = getRandomCard()
+// let secondCard = getRandomCard()
+// let cards = [firstCard, secondCard];
+// let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
 let isAlive = true
 let message = ""
@@ -10,18 +12,34 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 let newCardEl = document.getElementById("newCard-el");
 
-function startBtn(){
-    renderGame();
+
+
+function startBtn() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
 }
 
-function getRandomCard(){
-    return Math.floor(Math.random()* 10)+1
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    }
+    else if (randomNumber === 1) {
+        return 11
+    }
+    else {
+        return randomNumber
+    }
 }
 
-function renderGame(){
+function renderGame() {
     // cardsEl.textContent = "Cards: " + firstCard + " & " + secondCard; -- previuous line , now converting it into an array;
     cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++){
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + "  "
     }
     sumEl.textContent = "Sum: " + sum
@@ -35,15 +53,17 @@ function renderGame(){
         isAlive = false
     }
     messageEl.textContent = message
-}
+} 
 
-function newCardBtn(){
-    console.log("Drawing a new card from the deck");
-    let card = getRandomCard()
-    sum += card;
-    newCardEl.textContent = "Your new card is: " + card;
-    cards.push(card);
-    renderGame();
+function newCardBtn() {
+    // console.log("Drawing a new card from the deck");
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card;
+        newCardEl.textContent = "Your new card is: " + card;
+        cards.push(card);
+        renderGame();
+    }
 }
 
 
